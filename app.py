94421fe66583c65241da5c7996ccfa0c84e53ec3
Abhinav Bal <153614178+Abhinavvv0208@users.noreply.github.com>
2025-06-12 14:32:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,12 +37,14 @@ if __name__=="__main__":
     app.run()
 '''
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
 import util
 import os
 
 app = Flask(__name__)
-
+@app.route('/')
+def home():
+    return render_template('app.html')
 
 @app.route('/get_location_names')
 def get_location_names():
@@ -74,8 +76,8 @@ def predict_home_price():
     return response
 
 if __name__ == "__main__":
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     util.load_saved_artifacts()
     print("Starting Python Flask Server For House Price Prediction...")
     app.run()
